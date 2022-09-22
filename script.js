f34235d84b1cff58e029bfe1eb0ea516702281ed
Jackie He <jackiehe@Jackies-MacBook-Pro.local>
2022-09-21 23:32:26 -0700

    saving prog

--- script.js
+++ script.js
@@ -4,8 +4,9 @@ $(document).ready(function() {
     let result = document.querySelector(".result")
     let setup;
     let delivery;
+    let urlEnding = ["Programming"]
     function fetchJoke() {
-        fetch('https://v2.jokeapi.dev/joke/Programming')
+        fetch(`https://v2.jokeapi.dev/joke/${urlEnding.join()}`)
         .then((res) => res.json())
         .then((res) => {
             if (res.setup === undefined || res.delivery === undefined) {
@@ -27,4 +28,23 @@ $(document).ready(function() {
         result.innerHTML = '';
         fetchJoke();
     })
+    $('input.optional').change(function () {
+        let current = this.name;
+        let isChecked = $(`#${this.name}`)[0].checked;
+        console.log(isChecked);
+        if (isChecked == false) {
+            urlEnding.forEach((el, index) => {
+                if (el === this.name) {
+                    urlEnding.splice(index);
+                    console.log(urlEnding)
+                    fetchJoke();
+                }
+            })
+        } else {
+            console.log(current);
+            urlEnding.push(current);
+            console.log(urlEnding);
+            fetchJoke();
+        }
+    })  
 });
